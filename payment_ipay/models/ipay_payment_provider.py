6ac49a2a5d3913506fdd23d460ae11753ccacd4f
Jayliste <jaylisteledama94@gmail.com>
2023-08-02 22:01:56 +0300

    [ADD] show payment icon

--- payment_ipay/models/ipay_payment_provider.py
+++ payment_ipay/models/ipay_payment_provider.py
@@ -34,6 +34,7 @@ class iPayPaymentProvider(models.Model):
     show_credentials_page = fields.Boolean(string='Show Credentials Page', default=True)
     show_allow_express_checkout = fields.Boolean(string='Show Allow Express Checkout Supported', default=True)
     show_allow_tokenization = fields.Boolean(string='Show Allow Tokenization Supported', default=True)
+    show_payment_icon_ids = fields.Boolean(string='Show Payment Icon', default=True)
     
 
     #fixed values
