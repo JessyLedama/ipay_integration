7f14fdaa06902373797998fce0bb88acebe8ef26
Jayliste <jaylisteledama94@gmail.com>
2023-08-02 21:25:46 +0300

    [ADD] added supported tokenization

--- payment_ipay/models/ipay_payment_provider.py
+++ payment_ipay/models/ipay_payment_provider.py
@@ -24,6 +24,7 @@ class iPayPaymentProvider(models.Model):
     main_currency_id = fields.Many2one('res.currency', string="Currency")
     support_fees = fields.Boolean(string='Fees Supported', default=True)
     support_manual_capture = fields.Boolean(string='Manual Capture Supported', default=True)
+    support_tokenization = fields.Boolean(string='Tokenization Supported', default=True)
     
 
     #fixed values
