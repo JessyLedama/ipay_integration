669dd5fbe6ab51795ebcdccf1e29fd6c8a8c1c69
Jayliste <jaylisteledama94@gmail.com>
2023-08-03 07:37:34 +0300

    [ADD] added pre msg

--- payment_ipay/models/ipay_payment_provider.py
+++ payment_ipay/models/ipay_payment_provider.py
@@ -76,6 +76,8 @@ class iPayPaymentProvider(models.Model):
     redirect_form_view_id = fields.Many2one('ir.ui.view', string="Redirect From Template", help="The template rendering a form submitted to redirect the user when making a payment")
     token_inline_form_view_id = fields.Many2one('ir.ui.view', string="Token Inline Form Template")
     fees_int_var = fields.Float(string="Variable International Fees (%)")
+    pre_msg = fields.Html(string="Help Message")
+    
     
 
 
